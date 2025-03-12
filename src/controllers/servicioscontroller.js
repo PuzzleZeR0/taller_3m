8d96d326b862a1e615c201bc36bc09d1029ffde7
Ivan Roblero Rodriguez <145305247+PuzzleZeR0@users.noreply.github.com>
2025-03-12 13:02:50 -0600

    Delete src directory

--- src/controllers/servicioscontroller.js
+++
@@ -1,3 +0,0 @@
-exports.obtenerProductos = (req, res) => {
-    res.send('<h1>"servicios unach"</h1>');
-};
--- src/controllers/taller3m.controller.js
+++
@@ -1,9 +0,0 @@
-const { pool } = require("../models/db.js");
-
-const gettaller3m = async (req, res) => {
-    const [rows] = await pool.query('SELECT * FROM usuarios');
-    res.json(rows);
-};
-
-module.exports = { gettaller3m };
-
